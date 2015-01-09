b6635acaa457463837b3be9eb21f500dee3bbf74
Kiera <JustForLaughts66@gmail.com>
2015-01-09 09:19:47 -0800

    Milestone 4 currently

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -4,9 +4,26 @@
  * and open the template in the editor.
  */ 
  
- $("document").ready(function() {
+ $("document").ready(function() { 
      $(".Carrot").css("color", "blue"); 
-     $(".Rabbit").css("color","gold");
+     $(".Rabbit").css("color","gold"); 
+     $("h1").bind('mouseover', mouseOverMe).bind('mouseout', mouseOutMe); 
+     $('#1').bind('click', alertButtonClick);
+     $('#2').bind('click', "Are you late?");
  });
 
-
+function mouseOverMe() {
+    $("h1").html("Up");
+}
+ 
+function mouseOutMe(){
+    $("h1").html("What");
+} 
+ 
+function alertButtonClick(){
+    alert("Welcome");
+} 
+ 
+function Are you late?() {
+    
+}

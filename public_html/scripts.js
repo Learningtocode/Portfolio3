068e9902822aae6e0eba3e2be290afa057d5922b
Kiera <JustForLaughts66@gmail.com>
2015-01-21 09:20:15 -0800

    Worked on background  video

--- public_html/scripts.js
+++ public_html/scripts.js
@@ -3,13 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */  
-/*video#bgvid {
-            position: fixed; right: 0; bottom: 0;
-            min-width: 100%; min-height: 100%;
-            width: auto; height: auto; z-index: -100;
-            background: url(polina.jpg) no-repeat;
-            background-size: cover;
-              }*/
+
  
  $("document").ready(function() { 
      $('#superHuman').accordion({header: "h4"});
@@ -23,22 +17,6 @@
      $('#cat').bind('click', removeAPara); 
      $('#hidelogo').bind('click', hideTheImage);
      $('#showlogo').bind('click', showTheImage);   
-    /* $('#back').videoBG({
-		position:"fixed",
-		zIndex:-1,
-		opacity:0.5
-	});*/ 
-   /* $("a[rel=example_group]").fancybox({
-        'transitionIn': 'elastic', //elastic, fade, or none 
-        'transitionOut': 'elastic',
-        'titlePosition': 'over', // outside, inside or over 
-        'titleFormat': function(title, currentArray, currentIndex, currentOpts) {
-        return '<span id="fancybox-title-over">Image '+v (currentIndex + 1) + '/' 
-        + currentArray.length + (title.length ? '&nbsp; ' + title : '') + '</spam>';
-        } 
-    }); */
-    // setup ul.tabs to work as tabs for each div directly under div.panes
- // $("ul.tabs").tabs("div.panes > div");
  });
 
 function mouseOverMe() {
